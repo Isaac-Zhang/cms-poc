eecd2b6eefa9c389b800469056ab6cc87ac38874
Isaac Zhang <zhangpan0614@126.com>
2019-09-29 10:11:07 +0800

    create consumer start entering

---
+++ mscx-consumer/src/main/java/com/geekplus/cms/consumer/ConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.geekplus.cms.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/***
+ * 消费客户端启动类
+ *
+ * @Company GeekPlus
+ * @Project poc
+ * @Author <a href="mailto:zhangpan@geekplus.com.cn">Isaac.Zhang | 若初</a>
+ * @Date 2019/9/29
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
